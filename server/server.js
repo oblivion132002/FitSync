a6f662f180e87901f863c255c37ebd5e4f26c541
Christopher Simon <64184493+oblivion132002@users.noreply.github.com>
2026-05-20 22:14:43 +0400

    fixed mongodb connection

--- server/server.js
+++ server/server.js
@@ -11,17 +11,23 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("MongoDB Connected")
+})
+.catch((err) => {
+    console.log(err)
+})
+
+app.get("/", (req, res) => {
+    res.send("FitSync API Running")
+})
+
 app.use("/api/auth", authRoutes)
 app.use("/api/workouts", workoutRoutes)
 
-app.get("/", (req, res) => {
-  res.send("FitSync API Running")
-})
+const PORT = process.env.PORT || 5000
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err))
-
-app.listen(5000, () => {
-  console.log("Server running on port 5000")
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
 })
